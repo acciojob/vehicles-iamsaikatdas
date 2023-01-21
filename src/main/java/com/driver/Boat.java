bacadf3d813c5703a2a0e2f00161155223f43b14
Saikat <saikatdas9547@gmail.com>
2023-01-21 17:03:30 +0530

    First commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,2 +1,27 @@
-package com.driver;public class Boat {
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
 }
